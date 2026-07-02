91a60c6c6bbb9998269ed3c32350787b45aa75eb
Raaj Giri <raj.ash2493@gmail.com>
2026-07-02 16:56:12 +0530

    db related work

--- Backend/cmd/server/main.go
+++ Backend/cmd/server/main.go
@@ -4,8 +4,10 @@ import(
 	"fmt"
 
 	"github.com/raaj2493/Shortly/Backend/internal/config"
-	
+	"github.com/raaj2493/Shortly/Backend/internal/db"
 )
+	
+
 
 func main(){
 	// 1. Load the configuration
@@ -13,5 +15,14 @@ func main(){
 
 	// 2. Start the server
 	fmt.Printf("🚀 Starting URL Shortener API in %s mode on port %s...\n", cfg.Env, cfg.Port)
+
+	// 3. Connect to the database pool
+	databasePool := db.InitDB(cfg)
+	
+	// Ensure the connection pool closes gracefully when the application terminates
+	defer databasePool.Close()
+
+
+
 }
 
