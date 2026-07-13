0d28de768b358bfb201fb95446bf0fd0232a1213
Raaj Giri <raj.ash2493@gmail.com>
2026-07-13 08:52:00 +0530

    url repo

---
+++ Backend/internals/repository/url_repository.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/raaj2493/Shortly/Backend/internals/models"
+)
+
+type URLRepository struct {
+	db *sql.DB
+}
+
+func NewURLRepository(db *sql.DB) *URLRepository {
+	return &URLRepository{db: db}
+}
+
+func (r *URLRepository) CreateURL(ctx context.Context, originalURL , shortCode string) (*models.URL, error) {
+	query := `
+		INSERT INTO urls (original_url, short_code)
+		VALUES ($1, $2)
+		RETURNING id, created_at
+	`
+
+	var url models.URL
+	err := r.db.QueryRowContext(ctx, query, originalURL, shortCode).Scan(&url.ID, &url.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &url, nil
+}
+
+func (r *URLRepository) GetURL(ctx context.Context, shortCode string) (*models.URL, error) {
+	query := `
+		SELECT id, original_url, short_code, created_at
+		FROM urls
+		WHERE short_code = $1
+	`
+	var url models.URL
+	err := r.db.QueryRowContext(ctx, query, shortCode).Scan(&url.ID, &url.OriginalURL, &url.ShortCode, &url.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &url, nil
+}
