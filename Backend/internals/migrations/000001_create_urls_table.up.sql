51cca9136b74bf0fcc4fdb19ddbf887d14017a0f
Raaj Giri <raj.ash2493@gmail.com>
2026-07-13 08:23:24 +0530

    db migrations

---
+++ Backend/internals/migrations/000001_create_urls_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS urls (
+    id SERIAL PRIMARY KEY,
+    original_url TEXT NOT NULL,
+    short_code VARCHAR(6) UNIQUE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
