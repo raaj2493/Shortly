562ccb8eb3fc4758b5faff751179d5ba928cc31b
Raaj Giri <raj.ash2493@gmail.com>
2026-07-13 08:50:54 +0530

    url model

---
+++ Backend/internals/models/url_model.go
@@ -0,0 +1,11 @@
+package models
+
+
+import "time"
+
+type URL struct {
+	ID          int64     `json:"id"`
+	OriginalURL string    `json:"original_url"`
+	ShortCode   string    `json:"short_code"`
+	CreatedAt   time.Time `json:"created_at"`
+}
